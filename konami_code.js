341d306c5123fc0055789ab0d81794622d2a8fd5
Mohammed Chisti <mohammed.chisti@flatironschool.com>
2018-09-09 22:03:27 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -12,5 +12,32 @@ const codes = [
 ];
 
 function init() {
-  // your code here
+  const codes = [
+    "ArrowUp",
+    "ArrowUp",
+    "ArrowDown",
+    "ArrowDown",
+    "ArrowLeft",
+    "ArrowRight",
+    "ArrowLeft",
+    "ArrowRight",
+    "b",
+    "a"
+  ];
+
+  function init() {
+    let idx = 0
+  
+    document.body.addEventListener("keydown", (e) => {
+      const key = e.key
+      
+      idx = (codes[idx] === key) ? ++idx : 0
+      
+      if (idx === codes.length) {
+        window.alert("Hurray!");
+        idx = 0
+      }
+      
+    });
+  }
 }
