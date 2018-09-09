7c23c081cb75f3ed2503c6676ec3abbf387a7630
Mohammed Chisti <mohammed.chisti@flatironschool.com>
2018-09-09 22:05:20 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -17,7 +17,11 @@ function init() {
   document.body.addEventListener("keydown", (e) => {
     const key = e.key
     
-    idx = codes[idx] === key) ? ++idx : 0(
+    if(codes[idx] == key) {
+      idx++
+    } else {
+      idx = 0
+    }
     
     if (idx === codes.length) {
       window.alert("Hurray!");
